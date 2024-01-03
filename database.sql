f893634fc329e993091b4d9cde1b55e0f5b380b9
Matevž Kovačič <matevz5.kovacic@gmail.com>
2024-01-03 19:08:49 +0100

    db changes

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE notesapp;
 
 \c notesapp;
+CREATE DATABASE notesapp;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE t_users(
@@ -8,17 +8,20 @@ CREATE TABLE t_users(
   user_firstname VARCHAR(255) NOT NULL,
   user_lastname VARCHAR(255) NOT NULL,
   user_email VARCHAR(255) NOT NULL UNIQUE,
-  user_password VARCHAR(255) NOT NULL
+  user_password VARCHAR(255) NOT NULL,
+  CONSTRAINT unique_user_id UNIQUE (user_id),
+  CONSTRAINT unique_user_email UNIQUE (user_email)
 );
 
 CREATE TABLE t_notes(
   note_id SERIAL PRIMARY KEY,
   user_id UUID,
   title TEXT NOT NULL,
+  subject TEXT NULL,
   content TEXT NULL,
   note_version INTEGER DEFAULT 1,
   last_update TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at    ,
   FOREIGN KEY (user_id) REFERENCES t_users(user_id)
 );
 
@@ -26,12 +29,11 @@ CREATE TABLE t_attachments(
     attachment_id SERIAL PRIMARY KEY,
     note_id INTEGER,
     file_name TEXT NULL,
+    url TEXT NULL,
+    file_extension TEXT NULL,
+    file_original_name TEXT NULL,
     FOREIGN KEY (note_id) REFERENCES t_notes(note_id)
 );
-
-ALTER TABLE t_users
-ADD CONSTRAINT unique_user_id UNIQUE (user_id),
-ADD CONSTRAINT unique_user_email UNIQUE (user_email);
 
 CREATE TABLE t_shared_notes(
     share_id SERIAL PRIMARY KEY,
@@ -44,9 +46,7 @@ CREATE TABLE t_shared_notes(
     created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (note_id) REFERENCES t_notes(note_id),
     FOREIGN KEY (shared_by) REFERENCES t_users(user_id),
-    FOREIGN KEY (shared_with) REFERENCES t_users(user_id),
-    FOREIGN KEY (shared_with_email) REFERENCES t_users(user_email),
-    FOREIGN KEY (shared_by_email) REFERENCES t_users(user_email)
+    FOREIGN KEY (shared_with) REFERENCES t_users(user_id)
 );
 
 /* 0: owner   1: samo gleda    2:lahko edita */
